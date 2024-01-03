9b058d850e1012e7fef34d0091220295065a2057
leoworqu <leoworqu@gmail.com>
2024-01-03 01:46:43 -0800

    binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * @parent: Pointer to the node to insert the left-child in
  * @value: Value to store in the new node
  *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: Pointer to created node,or NULL on failure or if parent is NULL
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
