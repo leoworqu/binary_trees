8c0f953eb079552af8ad4aeb9cb1e22c77907644
leoworqu <leoworqu@gmail.com>
2024-01-06 02:05:52 -0800

    Aa

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,14 +10,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_node;
+	new_node= malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	new_node= malloc(sizeof(binary_tree_t));
 
 	new_node->n = value;
 	new_node->parent = parent;
